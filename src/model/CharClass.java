9db0a0fed5335f1a5755ee48ddda948081cf418d
tanne <tanne@DESKTOP-0CHF72S>
2020-11-01 22:01:42 -0700

    New object to store "Class" data
    Stores all the relevant info for a player class. Made to improve
    readability of the code.

---
+++ src/model/CharClass.java
@@ -0,0 +1,54 @@
+package model;
+
+import java.util.Arrays;
+
+public class CharClass {
+	private final int NUM_SAVING_THROWS = 2;
+	private String name;
+	private int numProficiencies;
+	private String[] savingThrows;
+	private String[] proficiencies;
+	
+	public CharClass(String name, int numProficiencies, String[] savingThrows, String[] proficiencies) {
+		this.name = name;
+		this.numProficiencies = numProficiencies;
+		this.savingThrows = savingThrows;
+		this.proficiencies = proficiencies;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumProficiencies() {
+		return numProficiencies;
+	}
+
+	public void setNumProficiencies(int numProficiencies) {
+		this.numProficiencies = numProficiencies;
+	}
+
+	public String[] getSavingThrows() {
+		return savingThrows;
+	}
+
+	public void setSavingThrows(String[] savingThrows) {
+		this.savingThrows = savingThrows;
+	}
+
+	public String[] getProficiencies() {
+		return proficiencies;
+	}
+
+	public void setProficiencies(String[] proficiencies) {
+		this.proficiencies = proficiencies;
+	}
+	
+	public String toString() {
+		return name + " " + Arrays.toString(savingThrows) + " " + Arrays.toString(proficiencies);
+	}
+}
