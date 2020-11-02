1fb3d989acf250a923792fab50e4e29045aa1b44
tanne <tanne@DESKTOP-0CHF72S>
2020-11-01 22:54:56 -0700

    Removed unused imports

--- src/model/CharacterSheet.java
+++ src/model/CharacterSheet.java
@@ -7,7 +7,6 @@ package model;
  */
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
